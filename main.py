21ccf6ff7c15c36c3f7231b4feccd3de56d940c2
Thisaraiva <111321528+Thisaraiva@users.noreply.github.com>
2023-07-07 15:41:46 -0300

    Update main.py
    Inclusao do metodo GET para buscar as url encurtadas no DB.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pyshorteners
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
-
 # Configurações
 app = Flask(__name__)
 
@@ -13,9 +12,6 @@ app.config['API_KEY'] = 'abc123xyz'
 
 # Cria uma instância do Redis
 redis_client = redis.Redis(host='localhost', port=6379)
-
-# Cria uma instância do Flask
-app = Flask(__name__)
 
 # Cria uma instância do cliente MongoDB
 mongo_client = MongoClient('mongodb://localhost:27017')
@@ -59,6 +55,27 @@ def encurtar():
         return jsonify({'url_encurtada': short_url})
     except pyshorteners.exceptions.ShorteningErrorException:
         return jsonify({'error': 'Ocorreu um erro ao encurtar a URL.'}), 500
+
+@app.route('/buscar', methods=['GET'])
+def buscar():
+    # Obtém o parâmetro "url" da query string
+    url = request.args.get('url')
+
+    if url:
+        # Verifica se a URL é a versão original ou encurtada
+        if url.startswith('http://') or url.startswith('https://'):
+            # Pesquisa pela URL original e retorna a URL encurtada, se encontrada
+            db_result = url_collection.find_one({'url': url})
+            if db_result:
+                return jsonify({'url_encurtada': db_result['url_encurtada']})
+        else:
+            # Pesquisa pela URL encurtada e retorna a URL original, se encontrada
+            db_result = url_collection.find_one({'url_encurtada': url})
+            if db_result:
+                return jsonify({'url_original': db_result['url']})
+
+    return jsonify({'error': 'URL não encontrada.'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
